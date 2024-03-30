03dffc31c00c1acbc77a920e35aae6fca54d8ad8
RantuBangal <rantubangal996@gmail.com>
2024-03-31 00:52:45 +0530

    Combined Pages

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
 import './Navbar.css'
 
 export default function Navbar() {
 
   const navigate = useNavigate();
+
+  const navigateToLogin = (e) => {
+    e.preventDefault()
+    navigate('/login')
+  }
+
+  const navigateToSignup = (e) => {
+    e.preventDefault()
+    navigate('/register')
+  }
 
   const navigateToProfile = (e) => {
     e.preventDefault()
@@ -22,8 +31,8 @@ export default function Navbar() {
             <Link className="active-link" to='/contact'>Contact Us</Link>
         </div>
         <div className="navbar-buttons-div" >
-            <button className="navbar-buttons" >Login</button>
-            <button className="navbar-buttons" >Signup</button>
+            <button className="navbar-buttons" onClick={e => navigateToLogin(e)} >Login</button>
+            <button className="navbar-buttons" onClick={e => navigateToSignup(e)} >Signup</button>
             <button className="navbar-buttons" onClick={e => navigateToProfile(e)} >Profile</button>
         </div>
 
